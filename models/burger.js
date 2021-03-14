bd261a1a2353677c9523a393a263bb714408ac8c
Austin Perez <austin.perez760@gmail.com>
2021-03-14 03:33:14 -0700

    fixed issue with orm and burger js functions being different names now orm can connect with js

--- models/burger.js
+++ models/burger.js
@@ -8,8 +8,8 @@ var burger = {
     });
   },
   // variables become arrays
-  insertOne: function (cols, vals, cb) {
-    orm.insertOne("burgers", cols, vals, function (res) {
+  addOne: function (cols, vals, cb) {
+    orm.addOne("burgers", cols, vals, function (res) {
       cb(res);
     });
   },
--- server.js
+++ server.js
@@ -1,21 +1,24 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
+var path = require("path");
 
 var PORT = process.env.PORT || 8080;
 var app = express();
 
 //Serve static content to the public directory
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 //parse JSON
 app.use(bodyParser.json());
-
+// View engine setup
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
 
 app.listen(PORT, function () {
   console.log("Server listening on localhost:" + PORT);
